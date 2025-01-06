5ea82116bb084b8f6df5c409854f37091843ee77
= <=>
2025-01-06 23:51:00 +0300

    testing list with 0 rotaions or n rotations where n is size of list

--- test_linear_rotated.py
+++ test_linear_rotated.py
@@ -1,7 +1,7 @@
 import pytest
 from linear_rotated import rotated_list_linear_sol
 
-#One rotation with positive numbers
+#One rotation with positive integers
 @pytest.mark.parametrize("numbers, rotation", [
     ([14, 0, 4, 5, 6, 8, 10], 1),
     ([8, 1, 3, 5, 7], 1),
@@ -23,3 +23,23 @@ def test_rotated_list_linear_sol_with_more_than_one_rotation(numbers, rotation):
 #Empty list
 def test_rotated_list_linear_sol_with_an_empty_list():
     assert rotated_list_linear_sol([]) == -1
+
+#One rotation with negative integers
+@pytest.mark.parametrize("numbers, rotation",[
+    ([-2, -12, -10, -8, -5, -4], 1),
+    ([-10, -14, -13, -12, -11], 1),
+    ([-20, -50, -45, -40, -35, -30, -25], 1)
+])
+def test_rotated_list_linear_sol_with_list_rotation_and_negative_ints(numbers, rotation):
+    assert rotated_list_linear_sol(numbers) == rotation
+
+
+#Test for list with n rotations or 0 rotations
+@pytest.mark.parametrize("numbers, rotation", [
+    ([2, 3, 4, 5, 6, 7], 0),
+    ([2], 0),
+    ([-10, -8, -7, -5, -3, -1], 0),
+    ([-4], 0)
+])
+def test_rotated_list_linear_sol_with_n_rotaions(numbers, rotation):
+    assert rotated_list_linear_sol(numbers) == rotation
