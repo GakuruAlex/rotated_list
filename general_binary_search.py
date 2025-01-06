e5124c949ffeb54a3a18edc95eb793358467d050
= <=>
2025-01-07 01:31:01 +0300

    general binary search #1 iteration

---
+++ general_binary_search.py
@@ -0,0 +1,16 @@
+from typing import List
+def general_binary_search(numbers: List[int], low: int, high: int, condition)-> int:
+   
+    while low <= high:
+        middle_index: int = (low + high) // 2
+        middle_number: int = numbers[middle_index]
+
+        result = condition(middle_number)
+
+        if result == 'found':
+            return middle_index + 1
+        elif result == 'left':
+            high = middle_index - 1
+        low = middle_index + 1
+    return -1
+
