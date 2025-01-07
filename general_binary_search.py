16faa1b1b908aa187dbd3c10498555e678340328
= <=>
2025-01-07 12:26:37 +0300

    use condition to check which half to check

--- general_binary_search.py
+++ general_binary_search.py
@@ -1,17 +1,18 @@
 from typing import List
-def general_binary_search(numbers: List[int], low: int, high: int, condition)-> int:
+def general_binary_search(low: int, high: int, condition)-> int:
    
     while low <= high:
         middle_index: int = (low + high) // 2
-        middle_number: int = numbers[middle_index]
-
-        result = condition(middle_number)
-
+        
+        result = condition(middle_index)
+        print(f"Middle index: {middle_index} low {low} high {high}")
         if result == 'found':
             return middle_index + 1
         elif result == 'left':
             high = middle_index - 1
-        else:
+        elif result == 'right':
             low = middle_index + 1
+        elif result == 'no rotation':
+            return 0
     return -1
 
